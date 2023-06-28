6841553ca1ab8ee78d2ed6f5d16cb711715cf7d8
Susil2510 <132734591+Susil2510@users.noreply.github.com>
2023-06-28 18:03:56 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/mksir12/tomvineet.git /DQ-_TOM
+  git clone https://github.com/Susil2510/MOVIE_AUTI.git /MOVIE_AUTI
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /DQ-_TOM
